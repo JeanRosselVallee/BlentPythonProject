97c5bbcde009d75554e41332706569bee1067af7
JeanRosselVallee <35218552+JeanRosselVallee@users.noreply.github.com>
2026-03-26 04:58:09 +0100

    Test a hello_world Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+'''
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_DATABASE_URI'] = 'sqlite:///project.db'
     db.init_app(app)
+    return app
+'''
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return '<h1>Hello, World!</h1><p>Your Flask app is running.</p>'
+
     return app
